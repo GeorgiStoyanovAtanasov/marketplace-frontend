55f47235b541ac65a5ce82c5cc6759d916aa4d11
MirkaaStefanov <mirkanstefanov2007@gmail.com>
2024-07-30 01:22:37 +0300

    submit in event client only have request body

--- demo/src/main/java/com/example/demo/clients/EventClient.java
+++ demo/src/main/java/com/example/demo/clients/EventClient.java
@@ -34,7 +34,7 @@ public interface EventClient {
     void apply(@RequestParam(name = "id") Integer id);
 
     @PostMapping("/submit")
-    public void postEvent(@RequestParam EventDTO eventDTO, BindingResult bindingResult);
+    public void postEvent(@RequestBody EventDTO eventDTO);
 }
 
 
