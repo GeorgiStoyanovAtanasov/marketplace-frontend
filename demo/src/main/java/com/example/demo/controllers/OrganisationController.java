551df0d422dfb1c795149c91b289d943c6f2cc99
MirkaaStefanov <mirkanstefanov2007@gmail.com>
2024-08-01 03:46:38 +0300

    add manager and organisation

--- demo/src/main/java/com/example/demo/controllers/OrganisationController.java
+++ demo/src/main/java/com/example/demo/controllers/OrganisationController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import com.example.demo.Services.AuthService;
 import com.example.demo.Services.OrganisationService;
 import com.example.demo.clients.OrganisationClient;
+import com.example.demo.dtos.ManagerDTO;
 import com.example.demo.dtos.OrganisationDTO;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,27 +27,26 @@ public class OrganisationController {
     }
 
     @GetMapping("/add")
-    public String addOrganisation(Model model) {
-        authService.getRoles(model);
+    public String addOrganisation(@ModelAttribute("id") Integer id, Model model) {
         model.addAttribute("organisationDTO", new OrganisationDTO());
+        model.addAttribute("id", id);
         return "organisation/organisation-form";
     }
 
     @PostMapping("/submit")
-    public String postOrganisation(@Valid @ModelAttribute OrganisationDTO organisationDTO, BindingResult bindingResult) {
+    public String postOrganisation(@Valid @ModelAttribute OrganisationDTO organisationDTO,@RequestParam(name = "id") Integer id, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "organisation/organisation-form";
         } else {
-            organisationClient.postOrganisation(organisationDTO);
-            return "redirect:/events";
+            if(!organisationClient.postOrganisation(organisationDTO,id)){
+                return "organisation/organisation-form";
+            }
+            return "redirect:/authentication/login";
         }
     }
 
     @GetMapping("/all")
     public String allOrganisations(Model model) {
-        if(!authService.hasSession()){
-            return "redirect:/authentication/login";
-        }
         authService.getRoles(model);
         model.addAttribute("allOrganisations", organisationService.allOrganisations());
         return "organisation/all-organisations";
