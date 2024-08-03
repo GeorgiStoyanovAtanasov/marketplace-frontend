63b67f3d054bdcae6ab901caa31b675c9c3dd40a
Eri1905 <ery.mtl1905@gmail.com>
2024-08-03 03:55:26 +0300

    update event type done

--- demo/src/main/java/com/example/demo/controllers/EventTypeController.java
+++ demo/src/main/java/com/example/demo/controllers/EventTypeController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controllers;
 
-import ch.qos.logback.core.model.Model;
+import com.example.demo.Services.AuthService;
+import com.example.demo.clients.EventClient;
+import com.example.demo.clients.EventTypeClient;
 import com.example.demo.dtos.EventTypeDTO;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +16,15 @@ import java.util.Optional;
 @Controller
 @RequestMapping("/event-type")
 public class EventTypeController {
+    AuthService authService;
+    EventTypeClient eventTypeClient;
+
+    @Autowired
+    public EventTypeController(AuthService authService, EventTypeClient eventTypeClient) {
+        this.authService = authService;
+        this.eventTypeClient = eventTypeClient;
+    }
+
     @GetMapping("/add")
     public void addEventType(Model model) {
 
@@ -32,10 +45,18 @@ public class EventTypeController {
 
     }
 
-    @PutMapping("/update")
-    public void updateEventType(@RequestParam("id") Integer id, @RequestBody EventTypeDTO eventTypeDTO) {
+    @GetMapping("/update/{id}")
+    public String updateEventType(@PathVariable Integer id, Model model) {
+        model.addAttribute("updateEventType", new EventTypeDTO());
+        model.addAttribute("eventTypeId", id);
+        authService.getRoles(model);
+        return "event-type/event-type-update-form";
+    }
 
-
+    @PostMapping("/update")
+    public String updateEventType(@RequestParam("id") Integer id, @ModelAttribute EventTypeDTO eventTypeDTO, Model model) {
+        eventTypeClient.updateEventType(id, eventTypeDTO);
+        return "redirect:/event-type/all";
     }
 }
 
