afb411288525c831ae10b2be09716d144c5ac427
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2024-08-13 02:03:50 +0300

    add validations for email and password. From binding results it's not showing the messages

--- demo/src/main/java/com/example/demo/dtos/RegisterUserDto.java
+++ demo/src/main/java/com/example/demo/dtos/RegisterUserDto.java
@@ -1,12 +1,17 @@
 package com.example.demo.dtos;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RegisterUserDto {
-
+    @NotEmpty(message = "email can not be empty")
     private String email;
+    @NotNull
     private String password;
+    @NotNull
     private String confirmPassword;
+    @NotEmpty
     private String fullName;
 }
